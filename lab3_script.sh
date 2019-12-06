856093eb86a2df3c4fd1becc1b0fcb7aa51d0134
corey <coco3010@colorado.edu>
2019-12-06 14:02:16 -0700

    Creating practice.sh file and storing regex_practice.sh

---
+++ lab3_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Author : Your Name
+# Date: Today's date
+# Script follows here:
+echo "Enter a file: "
+read f
+echo "Enter an expression: "
+read e
+grep $e $f
+grep -c ...-...-.... $f
+grep -c [.+@.+]...$ $f
+grep -o 303-...-.... $f
+grep .+@geocities.com $f
